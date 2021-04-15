d6b9e0a0eb24207825d2c3f1e2475d25ef1698bf
Mariana Costa <marianapatc@hotmail.com>
2021-04-15 15:40:38 +0100

    add link do github docs

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <header>
-      <h1>{{ title }}</h1>
-      <img
-        :src="require(`../../assets/icons/palette.svg`)"
-        :alt="`palleteicon`"
-      />
+      <div class="header__title-wrapper">
+        <h1>{{ title }}</h1>
+        <img
+          :src="require(`../../assets/icons/palette.svg`)"
+          :alt="`palleteicon`"
+        />
+      </div>
+      <a
+        rel="nofollow noopener noreferrer"
+        href="https://github.com/marianapatcosta/my-paint-app"
+        >help</a
+      >
     </header>
   </div>
 </template>
